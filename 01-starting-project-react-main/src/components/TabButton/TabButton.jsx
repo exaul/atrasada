5e4c985d64e335c8547b45467ef75010a257f6b6
exaul <137360873+exaul@users.noreply.github.com>
2024-06-19 23:30:14 -0500

    21 de mayo

---
+++ 01-starting-project-react-main/src/components/TabButton/TabButton.jsx
@@ -0,0 +1,12 @@
+export default function TabButton({children, isSelected, onClick}) {
+
+    return (
+        <li>
+            <button 
+            className={isSelected ? "active" : undefined}
+            onClick={onClick}>
+                {children}
+            </button>
+        </li>
+    );       
+}
